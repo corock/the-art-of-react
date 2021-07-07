e24ef3381d78405971115547d29cf8facaf893e8
corock <dycorphin@gmail.com>
2021-07-07 18:36:30 +0900

    Add comments for ReactDOM.render(...)

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/**
+ * 컴포넌트를 페이지에 렌더링하는 역할을 하며, react-dom 모듈을 불러와 사용할 수 있음.
+ * @param element   - 페이지에 렌더링할 내용을 JSX 형태로 작성
+ * @param container - 해당 JSX를 렌더링할 document 내부 요소 설정
+ */
 ReactDOM.render(
   <React.StrictMode>
     <App />
