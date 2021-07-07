e767ac3cc1301d5173ba781650da8c80386f4c38
corock <dycorphin@gmail.com>
2021-07-07 18:43:49 +0900

    Omit React.Fragment

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,10 +2,10 @@ import React, { Fragment } from 'react';
 
 function App() {
   return (
-    <Fragment>
+    <>
       <h1>리액트 안녕!</h1>
       <h2>잘 작동하니?</h2>
-    </Fragment>
+    </>
   );
 }
 
