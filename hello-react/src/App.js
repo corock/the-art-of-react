d3d4c98ec8df37d7d4955677fc76472325d2eb13
corock <dycorphin@gmail.com>
2021-07-08 19:44:34 +0900

    Add isRequired to make sure a warning is shown if the prop isn't provided

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -7,7 +7,14 @@ const App = () => {
    * `MyComponent`, expected `string`.
    */
   // return <MyComponent name={3}>리액트</MyComponent>;
-  return <MyComponent name={"React"}>리액트</MyComponent>;
+
+  /**
+   * Warning: Failed prop type: The prop `favoriteNumber` is marked as required
+   * in `MyComponent`, but its value is `undefined`.
+   */
+  // return <MyComponent name={"React"}>리액트</MyComponent>;
+
+  return <MyComponent name={"React"} favoriteNumber={1}>리액트</MyComponent>;
 };
 
 export default App;
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -3,12 +3,14 @@ import React from 'react';
 // 컴포넌트의 필수 props 를 지정하거나 props의 타입을 지정할 때 사용
 import PropTypes from 'prop-types';
 
-const MyComponent = ({ name, children }) => {
+const MyComponent = ({ name, favoriteNumber, children }) => {
   return (
     <div>
       안녕하세요, 제 이름은 {name}입니다.
       <br />
       children 값은 {children} 입니다.
+      <br />
+      제가 좋아하는 숫자는 {favoriteNumber}입니다.
     </div>
   );
 };
@@ -18,7 +20,8 @@ MyComponent.defaultProps = {
 };
 
 MyComponent.propTypes = {
-  name: PropTypes.string
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired
 };
 
 // 다른 파일에서 이 파일을 import 할 때, 위에서 선언한 MyComponent 클래스를 로드
