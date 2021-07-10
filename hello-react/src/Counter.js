def95622d1f8ee7bef8f67eb1da453351df914e8
corock <dycorphin@gmail.com>
2021-07-10 19:53:18 +0900

    Add fixed number

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -11,16 +11,18 @@ class Counter extends Component {
     // state 초깃값 설정
     this.state = {
       number: 0,
+      fixedNumber: 0
     };
   }
 
   render() {
     // state 를 조회할 때는 this.state 로 조회합니다.
-    const { number } = this.state;
+    const { number, fixedNumber } = this.state;
 
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           // onClick 을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
           onClick={() => {
