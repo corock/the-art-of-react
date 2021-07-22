e68ec8b7abf23ea8ee6951bd856f6ab910a96064
corock <dycorphin@gmail.com>
2021-07-22 19:52:50 +0900

    Done chapter 4

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,14 +1,27 @@
 import React, { useState } from 'react';
 
 const EventPractice = () => {
-  const [username, setUsername] = useState('');
-  const [message, setMessage] = useState('');
-  const onChangeUsername = (e) => setUsername(e.target.value);
-  const onChangeMessage = (e) => setMessage(e.target.value);
+  const [form, setForm] = useState({
+    username: '',
+    message: '',
+  });
+  const { username, message } = form;
+  const onChange = (e) => {
+    const nextForm = {
+      // 기존의 form 내용을 이 자리에 복사한 뒤
+      ...form,
+
+      // 원하는 값을 덮어 씌우기
+      [e.target.name]: e.target.value,
+    };
+    setForm(nextForm);
+  };
   const onClick = () => {
     alert(username + ': ' + message);
-    setUsername('');
-    setMessage('');
+    setForm({
+      username: '',
+      message: '',
+    });
   };
   const onKeyPress = (e) => {
     if (e.key === 'Enter') {
@@ -24,14 +37,14 @@ const EventPractice = () => {
         name="username"
         placeholder="사용자명"
         value={username}
-        onChange={onChangeUsername}
+        onChange={onChange}
       />
       <input
         type="text"
         name="message"
         placeholder="아무거나 입력해 보세요"
         value={message}
-        onChange={onChangeMessage}
+        onChange={onChange}
         onKeyPress={onKeyPress}
       />
       <button onClick={onClick}>확인</button>
