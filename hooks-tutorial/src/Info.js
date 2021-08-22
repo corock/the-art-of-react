eab0adb8b0dde8c82089304a035a71f20b523f16
corock <dycorphin@gmail.com>
2021-08-23 08:03:44 +0900

    Add using the effect hook

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,8 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Info = () => {
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
+
+  /**
+   * @function useEffect - 기본적으로 렌더링되고 난 직후마다 실행되며,
+   * 두 번째 파라미터 배열에 무엇을 넣는지에 따라 실행되는 조건이 달라집니다.
+   */
+  useEffect(() => {
+    // console.log("렌더링이 완료되었습니다!");
+    // console.log({ name, nickname });
+
+    console.log("effect");
+    console.log(name);
+    return () => {
+      console.log("cleanup");
+      console.log(name);
+    };
+  }, [name]);
 
   const onChangeName = (e) => {
     setName(e.target.value);
