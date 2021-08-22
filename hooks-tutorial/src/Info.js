10fe5a3d7b345a0044b31eb010e30ed0fc328165
corock <dycorphin@gmail.com>
2021-08-23 08:21:26 +0900

    Add managing input state

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,37 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-
-  /**
-   * @function useEffect - 기본적으로 렌더링되고 난 직후마다 실행되며,
-   * 두 번째 파라미터 배열에 무엇을 넣는지에 따라 실행되는 조건이 달라집니다.
-   */
-  useEffect(() => {
-    // console.log("렌더링이 완료되었습니다!");
-    // console.log({ name, nickname });
-
-    console.log('effect');
-    console.log(name);
-    return () => {
-      console.log('cleanup');
-      console.log(name);
-    };
-  }, [name]);
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
+  const [state, dispatch] = useReducer(reducer, {
+    name: '',
+    nickname: '',
+  });
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    dispatch(e.target);
   };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
