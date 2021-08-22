801aa25bd69cf581a05f8e77abf1f7df8920bc1d
corock <dycorphin@gmail.com>
2021-08-23 07:53:05 +0900

    Add using the state hook

---
+++ hooks-tutorial/src/Counter.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  // [사태 값, 상태를 설정하는 함수]
+  const [value, setValue] = useState(0);
+
+  return (
+    <div>
+      <p>
+        현재 카운터 값은 <b>{value}</b>입니다.
+      </p>
+      <button onClick={() => setValue(value + 1)}>+1</button>
+      <button onClick={() => setValue(value - 1)}>-1</button>
+    </div>
+  );
+};
+
+export default Counter;
